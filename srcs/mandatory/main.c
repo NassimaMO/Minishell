1b9b344c10e253e2f1414a49919ccd93b1ea7f29
Nassima MOUSLIM <nassimamouslim@gmail.com>
2023-01-09 18:30:17 +0100

    commands

--- srcs/mandatory/main.c
+++ srcs/mandatory/main.c
@@ -6,19 +6,21 @@
 /*   By: nmouslim <nmouslim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 13:02:38 by nghulam-          #+#    #+#             */
-/*   Updated: 2023/01/09 15:34:01 by nmouslim         ###   ########.fr       */
+/*   Updated: 2023/01/09 15:45:01 by nmouslim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+int	main(int argc, char *argv[], char *envp[])
 {
+	(void)argc;
+	(void)argv;
 	while (1)
 	{
 		print_shell();
 		control_c();
-		get_input();
+		handle_cmd(get_input(), envp);
 	}
 	return (0);
 }
