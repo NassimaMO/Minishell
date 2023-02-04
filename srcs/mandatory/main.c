832822ac10d8f41ea5e63c62e9b2614c75878510
Nily GHULAM NABI <nghulam-@e3r6p1.clusters.42paris.fr>
2023-02-04 16:09:32 +0100

    updated redirect.c to add << (heredoc)

--- srcs/mandatory/main.c
+++ srcs/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: nmouslim <nmouslim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 13:02:38 by nghulam-          #+#    #+#             */
-/*   Updated: 2023/02/03 17:18:56 by nmouslim         ###   ########.fr       */
+/*   Updated: 2023/02/02 13:41:54 by nmouslim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,23 +42,8 @@ void	ft_close(int nb, ...)
 }
 
 /* TO DO LIST :
-- executable with relative or absolute path : may leak with SIGINT
 - proper history
-- file redirection : <<
-- built_in variable "$?" (last exit code) */
-
-char	**ft_split_dup(char **split)
-{
-	char	**to_return;
-	int		i;
-
-	to_return = malloc(sizeof(char *) * split_len(split));
-	i = -1;
-	while (split[++i])
-		to_return[i] = ft_strdup(split[i]);
-	to_return[i] = NULL;
-	return (to_return);
-}
+*/
 
 int	main(void)
 {
@@ -69,19 +54,15 @@ int	main(void)
 	history = NULL;
 	exit_code = 0;
 	signals();
-	set_terminal(SET);
 	init_env();
 	while (1)
 	{
 		print_shell();
-		input = get_input(history);
-		if (handle_cmd(input, &exit_code, ft_split_dup(history)) == EXIT)
+		input = get_input(&history);
+		if (handle_cmd(input, &exit_code, history) == EXIT)
 			break ;
-		if (input)
-			history = add_split(history, input);
 	}
 	free_env();
 	free_split(history);
-	set_terminal(RESET);
 	return (exit_code);
 }
