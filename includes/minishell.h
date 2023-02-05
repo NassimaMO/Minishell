a8739ab7b2624e036784fa6063c0a4f49a683b74
Nassima MOUSLIM <nassimamouslim@gmail.com>
2023-02-05 15:38:40 +0100

    just put the prototypes in a more coherent order

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nmouslim <nmouslim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 13:05:38 by nghulam-          #+#    #+#             */
-/*   Updated: 2023/02/05 15:24:56 by nmouslim         ###   ########.fr       */
+/*   Updated: 2023/02/05 15:37:51 by nmouslim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,19 +47,63 @@
 
 extern char	**environ;
 
-/* redirect.c */
-void	redirect_stdin(char *str, int fd[2]);
-void	redirect_stdout(char *str, int fd[2]);
+/* main.c */
+void	ft_close(int nb, ...);
+
+/* signals.c */
+void	signals(void);
+
+/* print.c */
+char	*get_cmd(char *cmd);
+void	print_env(char **envp);
+void	print_export(char **envp);
+void	print_shell(void);
+void	print_error(const char *cmd, const char *error);
+
+/* terminal.c */
+void	set_terminal(int option);
+
+/* input.c */
+char	*get_input(char **history);
+
+/* manage_input.c */
+void	process_input(char **history, char *buff, size_t *moves, \
+					size_t *cursor);
+
+/* split.c */
+char	**ft_split_dup(char **split);
+char	**add_split(char **split, char *str);
+void	free_split(char **split);
+size_t	split_len(char **split);
+void	free_env(void);
 
 /* ft_split_set.c */
 char	**ft_split_set(char *str, char *charset);
 
-/* echo.c */
-char	*get_processed_input(char *input, int opt, int exit_code);
-int		echo_cmd(char *input, int exit_code);
+/* commands.c */
+int		is_built_in(char *cmd);
+void	built_in(char *input, int fd_in, int fd_out, int *exit_code);
+int		handle_cmd(char *input, int *exit_code, char **history);
 
 /* quotes.c */
 int		quote_gestion(char *input, char **output, int i, int exit_code);
+
+/* pipex.c */
+void	exec_cmd(char *cmd, int fd_i, int fd_o, char **h);
+int		ft_pipes(int n, char **cmds, int fd[2], char **h);
+
+/* pipex_utils.c */
+char	*get_pathname(char *cmd, char *envp[]);
+char	**get_cmd_args(const char *cmd);
+char	*relative_path(char *path);
+
+/* redirect.c */
+void	redirect_stdin(char *str, int fd[2]);
+void	redirect_stdout(char *str, int fd[2]);
+
+/* echo.c */
+char	*get_processed_input(char *input, int opt, int exit_code);
+int		echo_cmd(char *input, int exit_code);
 
 /* path.c */
 int		pwd_cmd(char *input);
@@ -72,51 +116,7 @@ int		export_cmd(char *str, int exit_code);
 int		unset_cmd(char *line, int exit_code);
 int		env_cmd(char *input);
 
-/* terminal.c */
-void	set_terminal(int option);
-
-/* input.c */
-char	*get_input(char **history);
-
 /* exit.c */
 int		check_exit(char *input, int *exit_code);
-
-/* manage_input.c */
-void	process_input(char **history, char *buff, size_t *moves, \
-					size_t *cursor);
-
-/* print.c */
-char	*get_cmd(char *cmd);
-void	print_env(char **envp);
-void	print_export(char **envp);
-void	print_shell(void);
-void	print_error(const char *cmd, const char *error);
-
-/* signals.c */
-void	signals(void);
-
-/* split.c */
-char	**ft_split_dup(char **split);
-char	**add_split(char **split, char *str);
-void	free_split(char **split);
-size_t	split_len(char **split);
-void	free_env(void);
-
-/* main.c */
-void	ft_close(int nb, ...);
-
-/* commands.c */
-int		is_built_in(char *cmd);
-void	built_in(char *input, int fd_in, int fd_out, int *exit_code);
-int		handle_cmd(char *input, int *exit_code, char **history);
-
-/* pipex.c */
-void	exec_cmd(char *cmd, int fd_i, int fd_o, char **h);
-int		ft_pipes(int n, char **cmds, int fd[2], char **h);
-
-/* pipex_utils.c */
-char	*get_pathname(char *cmd, char *envp[]);
-char	**get_cmd_args(const char *cmd);
-char	*relative_path(char *path);
 
 #endif
