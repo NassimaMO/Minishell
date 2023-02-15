c0b5b977ef11e416a7019eadf28b5612bfa86919
Nassima MOUSLIM <nassimamouslim@gmail.com>
2023-02-15 12:03:49 +0100

    wait a minute

--- srcs/mandatory/history.c
+++ srcs/mandatory/history.c
@@ -1,18 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   history.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: nmouslim <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/15 09:42:07 by nmouslim          #+#    #+#             */
-/*   Updated: 2023/02/15 09:42:11 by nmouslim         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
-static void	print_prompt_hist(size_t *cursor, size_t *moves, char **history)
+static void		print_prompt_hist(size_t *cursor, size_t *moves, char **history)
 {
 	ft_printf("\033[J\r");
 	print_shell(0);
@@ -20,21 +8,19 @@ static void	print_prompt_hist(size_t *cursor, size_t *moves, char **history)
 	*cursor = ft_strlen(history[split_len(history) - (*moves + 1)]);
 }
 
-void	go_through_hist(size_t *cursor, size_t *moves, char **history, int len)
+void		go_through_hist(size_t *cursor, size_t *moves, char **history, int len)
 {
 	struct winsize	w;
 	int				x;
 	int				y;
-
+	
 	get_cursor_pos((ioctl(0, TIOCGWINSZ, &w), &x), &y);
 	if (split_len(history) >= (*moves + 1) && y == w.ws_row && \
 		len + print_shell(LEN) > w.ws_col)
 	{
-		while (len + print_shell(LEN) / w.ws_col > 0 && len > 0)
-		{
-			ft_printf("\033[1K\033[A");
-			len -= w.ws_col;
-		}
+		while (len + print_shell(LEN) / w.ws_col > 0 && len > 0 && \
+				len + print_shell(LEN) > w.ws_row)
+			len -= w.ws_col, ft_printf("\033[1K\033[A");
 		print_prompt_hist(cursor, moves, history);
 	}
 	else if (split_len(history) >= (*moves + 1))
